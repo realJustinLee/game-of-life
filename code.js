7045bee8931e7a44dccdc6316c16d12c1b6794a5
Li Xin <JustinDellAdam@live.com>
2020-04-29 22:48:17 +0800

    Created the controllers

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
 let rows = 32;
 let cols = 32;
 
+let playing = false;
+
 // initialize
 function initialize() {
     createTable();
+    setupControlButtons();
 }
 
 // lay out the board
@@ -37,6 +40,45 @@ function callClickHandler() {
     } else {
         this.setAttribute("class", "live");
     }
+}
+
+function setupControlButtons() {
+    // button to start
+    let startButton = document.getElementById("start");
+    startButton.onclick = startButtonHandler;
+
+    // button to clear
+    let clearButton = document.getElementById("clear");
+    clearButton.onclick = clearButtonHandler;
+}
+
+function clearButtonHandler() {
+    console.log("Clear the game: stop playing, clear the grid");
+    playing = false;
+    let startButton = document.getElementById("start")
+    startButton.innerHTML = "start"
+
+    // TODO: code here
+    //  the action that clears the grid map
+}
+
+function startButtonHandler() {
+    if (playing) {
+        console.log("Pause the game");
+        playing = false
+        this.innerHTML = "continue";
+    } else {
+        console.log("Continue the game");
+        playing = true
+        this.innerHTML = "pause";
+        play();
+    }
+}
+
+function play() {
+    console.log("Play the game");
+    // TODO: code here
+    //  the game of life module
 }
 
 // start everything
